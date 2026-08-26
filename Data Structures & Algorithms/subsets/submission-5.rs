cc8cf92271d8c522f5088be0685021fba8c1f7d2
Soham Upadeo <sohamup13@gmail.com>
2026-08-26 08:36:12 +0530

    Add: subsets - submission-5

---
+++ Data Structures & Algorithms/subsets/submission-5.rs
@@ -0,0 +1,32 @@
+impl Solution {
+    pub fn backtrack(r: &mut Vec<Vec<i32>>, i: usize, arr: &Vec<i32>,subset: &mut Vec<i32>){
+        if i>=arr.len(){
+            r.push(subset.clone());
+            return;
+        }
+        subset.push(arr[i]);
+        Self::backtrack(r,i+1,arr,subset);
+        subset.pop();
+        Self::backtrack( r,i+1,arr,subset);
+
+    }  
+    pub fn subsets(nums: Vec<i32>) -> Vec<Vec<i32>> {
+        let mut results = vec![];
+        let mut subset =  Vec::new();
+
+        Self::backtrack(&mut results,0,&nums,&mut subset);
+
+        results
+    }
+
+     
+}
+
+/*
+
+
+backtracking - i,arr,results
+
+
+
+*/
